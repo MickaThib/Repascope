f962356455c04f40029ad913c162bd32a65aadbb
Mickael Thibouret <mickael.thibouret@gmail.com>
2026-05-04 12:06:24 +0200

    Refonte de la base de l'écran MealsManager

---
+++ Repascope/Helpers/ListLabelType.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Repascope
+//
+//  Created by Mickael Thibouret on 04/05/2026.
+//
+
---
+++ Repascope/MealsManagerComponents/MealsManagerList.swift
@@ -0,0 +1,18 @@
+//
+//  MealsManagerList.swift
+//  Repascope
+//
+//  Created by Mickael Thibouret on 04/05/2026.
+//
+
+import SwiftUI
+
+struct MealsManagerList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealsManagerList()
+}
